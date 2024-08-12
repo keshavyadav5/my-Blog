289402ea75040928b1ba9807c95302d71bf5d401
keshavyadav5 <kesavyadav992@gmail.com>
2024-08-13 00:08:12 +0530

    create signin routes

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,6 +1,7 @@
 const User = require('../models/user.model')
 const bcrypt = require('bcryptjs');
 const { errorHandler } = require('../utils/error');
+const jwt = require('jsonwebtoken')
 
 const SignUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -26,7 +27,38 @@ const SignUp = async (req, res, next) => {
   }
 }
 
+const SignIn = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(errorHandler(400, 'Email or password is incorrect'));
+    }
+
+    const isMatch = bcrypt.compareSync(password, user.password);
+    if (!isMatch) {
+      return next(errorHandler(400, 'Email or password is incorrect'));
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
+      expiresIn: '1h',
+    });
+
+    const { password: pass, ...rest } = user._doc
+    res.cookie("access_token", token, { httpOnly: true }).status(200).json({
+      success: true,
+      message: "Login successfull",
+      rest
+    })
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+
 module.exports = {
   SignUp,
-
+  SignIn,
 }
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const { SignUp } = require('../controller/auth.controller')
+const { SignUp, SignIn } = require('../controller/auth.controller')
 const router = express.Router()
 
 router.post('/signup',SignUp)
+router.post('/signin',SignIn)
 
 module.exports = router
