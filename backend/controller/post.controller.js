980b29ce279e5bbcda17ebfdc6771c4a98a8bf0e
keshavyadav5 <kesavyadav992@gmail.com>
2024-09-02 01:44:23 +0530

    created backend to get all posts

--- backend/controller/post.controller.js
+++ backend/controller/post.controller.js
@@ -1,5 +1,5 @@
-const Post = require('../models/post.model.js') ;
-const { errorHandler } = require('../utils/error.js') ;
+const Post = require('../models/post.model.js');
+const { errorHandler } = require('../utils/error.js');
 
 const create = async (req, res, next) => {
   if (!req.user.isAdmin) {
@@ -12,7 +12,7 @@ const create = async (req, res, next) => {
     .split(' ')
     .join('-')
     .toLowerCase()
-    .replace(/[^a-zA-Z0-9-]/g,'');
+    .replace(/[^a-zA-Z0-9-]/g, '');
   const newPost = new Post({
     ...req.body,
     slug,
@@ -25,6 +25,43 @@ const create = async (req, res, next) => {
     next(error);
   }
 };
+
+const getposts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === 'asc' ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { userId: req.query.category }),
+      ...(req.query.slug && { userId: req.query.slug }),
+      ...(req.query.postId && { userId: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: 'i' } },
+          { content: { $regex: req.query.searchTerm, $options: 'i' } }
+        ]
+      }),
+    }).sort({ updateAt: sortDirection }).skip(startIndex).limit(limit);
+
+    const totalPosts = await Post.countDocuments()
+    const now = new Date()
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    )
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo }
+    })
+    res.status(200).json({ posts, totalPosts, lastMonthPosts });
+  } catch (error) {
+    next(error)
+  }
+}
+
 module.exports = {
   create,
+  getposts
 };
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,8 +1,9 @@
 const expresss = require('express');
 const verifyToken = require('../utils/verifyUser');
-const { create } = require('../controller/post.controller');
+const { create, getposts } = require('../controller/post.controller');
 const router = expresss.Router()
 
 router.post('/create',verifyToken,create);
+router.get('/getposts',getposts)
 
 module.exports = router
